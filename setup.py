dfce3c1eaa75c9a3a0eda4f89ad44a3dd5ba2356
Ruslan Spivak <ruslan.spivak@gmail.com>
2012-03-12 00:32:49 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(*rel_names):
 
 setup(
     name='crammit',
-    version='0.2',
+    version='0.3',
     url='https://github.com/rspivak/crammit.git',
     license='MIT',
     description=('Asset packaging library. '
